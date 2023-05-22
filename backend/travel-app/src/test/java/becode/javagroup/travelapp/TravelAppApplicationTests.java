5d0ed80504670ceafb747e1b76b0e54b667901dd
Pious Alpha <102190049+Piouscode@users.noreply.github.com>
2023-05-22 16:57:44 +0200

    Added backend, README and .gitignore files

---
+++ backend/travel-app/src/test/java/becode/javagroup/travelapp/TravelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package becode.javagroup.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
