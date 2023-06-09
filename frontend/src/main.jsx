44f9cadc1e90605525626c4a236ba1ddaa0573e7
Marieke Malfait <malfait99+becode@gmail.com>
2023-06-09 09:36:27 +0200

    Clean up file structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
-import "./init.js";
+import "./js/init";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import ChatRoom from "./components/Chat.jsx";
+import ChatRoom from "./components/chat";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
