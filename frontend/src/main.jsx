33b00571043e223f669a3d6c0bf7a4bd654c6d59
AmaaniL <malfait99+github@gmail.com>
2023-06-18 13:32:54 +0200

    Update package.json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <AuthProvider>
-        <App/>
-        <ChatRoom />
+      <App />
     </AuthProvider>
-</React.StrictMode>,)
+  </React.StrictMode>
+);
